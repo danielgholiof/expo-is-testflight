8c17d8c148a86948da26dff813e34fc8c5e5d633
Daniel Gholiof <daniel.gholiof@swace.se>
2026-01-12 15:30:56 +0100

    Remove exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-// Reexport the native module. On web, it will be resolved to ExpoIsTestflightModule.web.ts
-// and on native platforms to ExpoIsTestflightModule.ts
 export { default } from './ExpoIsTestflightModule';
-export { default as ExpoIsTestflightView } from './ExpoIsTestflightView';
-export * from  './ExpoIsTestflight.types';
